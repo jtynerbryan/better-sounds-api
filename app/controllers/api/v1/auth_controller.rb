708d79bf5ea6d758b7b1b6f18e03d80dfaea386d
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-09-20 18:00:46 -0400

    first commit

---
+++ app/controllers/api/v1/auth_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::AuthController < ApplicationController
+
+  # skip_before_action :authorized, only: [:spotify_request]
+
+  def spotify_request
+    # User has clicked "login" button - assemble get request to Spotify to have
+    # user authorize application
+    query_params = {
+      client_id: ENV['CLIENT_ID'],
+      response_type: "code",
+      redirect_uri: ENV['REDIRECT_URI'],
+      scope: "user-library-read user-top-read user-read-recently-played playlist-read-private",
+      show_dialog: true
+    }
+    url = "https://accounts.spotify.com/authorize/"
+    # redirects user's browser to Spotify's authorization page, which details
+    # scopes my app is requesting
+    redirect_to "#{url}?#{query_params.to_query}"
+  end
+
+  def show
+  # if application_controller#authorized is successful, return data for user
+    # render json: current_user
+    render json: {username: current_user.username,
+                  spotify_url: current_user.spotify_url,
+                  profile_img_url: current_user.profile_img_url}
+  end
+end
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::UsersController < ApplicationController
+  def show
+    user = User.find(1)
+
+    render json: {user: user}
+  end
+
+  def create
+    # handle response from Spotify
+    if params[:error]
+      puts "Login Error", params
+
+      # if login failed, redirect to front-end error message
+      redirect_to "http://localhost:3001/failure"
+    else
+      # if login succeeded, assemble and send request to Spotify for access and refresh tokens
+      body = {
+        grant_type: "authorization_code",
+        code: params[:code],
+        redirect_uri: ENV['REDIRECT_URI'],
+        client_id: ENV['CLIENT_ID'],
+        client_secret: ENV['CLIENT_SECRET']
+      }
+
+      auth_response = RestClient.post('https://accounts.spotify.com/api/token', body)
+      #convert response.body to JSON for assignment
+      auth_params = JSON.parse(auth_response.body)
+      #assemble and send request to Spotify for user profile information
+      header = {
+        Authorization: "Bearer #{auth_params["access_token"]}"
+        }
+
+      user_response = RestClient.get("https://api.spotify.com/v1/me", header)
+
+      #convert response.body to JSON for assignment
+      user_params = JSON.parse(user_response.body)
+
+      #Create new user based on response, or find existing user in database
+      user = User.find_or_create_by(username: user_params["id"],
+                        spotify_url: user_params["external_urls"]["spotify"],
+                        href: user_params["href"],
+                        uri: user_params["uri"])
+      user.update(access_token: auth_params["access_token"], refresh_token: auth_params["refresh_token"])
+
+      user.refresh_access_token
+
+      #redirect to React front-end
+      redirect_to "http://localhost:3001/success"
+    end
+
+  end
+
+end
