8574c0ec8f0217eb4ed12f763671d1990880fbf9
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-09-23 17:19:39 -0400

    changed tracks routes to 'post', limit of 50 tracks in request to spotify, succesfully sending tracks to the frontend

--- app/controllers/api/v1/tracks_controller.rb
+++ app/controllers/api/v1/tracks_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::TracksController < ApplicationController
 
     def top_tracks
-      user  = User.find(1)
+      @user  = User.find(params[:id])
 
       header = {
-          Authorization: "Bearer #{user.access_token}"
+          Authorization: "Bearer #{@user.access_token}"
       }
 
-      tracks_response = RestClient.get("https://api.spotify.com/v1/me/top/tracks", header)
+      tracks_response = RestClient.get("https://api.spotify.com/v1/me/top/tracks?limit=50", header)
       tracks_to_send = JSON.parse(tracks_response.body)
       render json: {tracks: tracks_to_send}
     end
@@ -19,7 +19,7 @@ class Api::V1::TracksController < ApplicationController
           Authorization: "Bearer #{user.access_token}"
       }
 
-      tracks_response = RestClient.get("https://api.spotify.com/v1/me/player/recently-played", header)
+      tracks_response = RestClient.get("https://api.spotify.com/v1/me/player/recently-played?limit=50", header)
       tracks_to_send = JSON.parse(tracks_response.body)
       render json: {tracks: tracks_to_send}
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
         get 'login', :to => 'auth#spotify_request'
         post '/users/create', :to => 'users#create'
         get '/users/show', :to => 'users#show'
-        get '/top_tracks', :to => 'tracks#top_tracks'
-        get 'recently_played_tracks', :to => 'tracks#recently_played_tracks'
+        post '/top_tracks', :to => 'tracks#top_tracks'
+        post 'recently_played_tracks', :to => 'tracks#recently_played_tracks'
       end
   end
 end
