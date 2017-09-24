818296c6d9c610fa18299a7fdd551828ff46c250
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-09-24 19:57:32 -0400

    hitting spotify api for audio features and sending to frontend

--- app/controllers/api/v1/audio_features_controller.rb
+++ app/controllers/api/v1/audio_features_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::AudioFeaturesController < ApplicationController
         Authorization: "Bearer #{@user.access_token}"
     }
 
-    audio_features_response = RestClient.get(`https://api.spotify.com/v1/audio_features/ids=#{params[:ids]}`, header)
-    audio_features_to_send = JSON.parse(tracks_response.body)
-    render json: {tracks: audio_features_to_send}
+    audio_features_response = RestClient.get("https://api.spotify.com/v1/audio-features/?ids=" + params[:ids], header)
+    audio_features_to_send = JSON.parse(audio_features_response)
+    render json: {features: audio_features_to_send}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
       namespace :v1 do
-        get 'login', :to => 'auth#spotify_request'
+        get '/login', :to => 'auth#spotify_request'
         post '/users/create', :to => 'users#create'
         get '/users/show', :to => 'users#show'
         post '/top_tracks', :to => 'tracks#top_tracks'
-        post 'recently_played_tracks', :to => 'tracks#recently_played_tracks'
-        post 'audio_features', :to => 'audio_features#audio_features'
+        post '/recently_played_tracks', :to => 'tracks#recently_played_tracks'
+        post '/audio_features', :to => 'audio_features#audio_features'
       end
   end
 end
