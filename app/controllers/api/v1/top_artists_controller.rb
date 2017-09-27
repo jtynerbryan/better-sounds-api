4734c6016dcfea5fca77b007cc59333e2dbaf2ce
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-09-27 18:17:27 -0400

    created top_artists controller, hitting Spotify api and sending data to frontend

---
+++ app/controllers/api/v1/top_artists_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::TopArtistsController < ApplicationController
+
+    def top_artists
+      @user = User.find(params[:user_id])
+
+      header = {
+          Authorization: "Bearer #{@user.access_token}"
+      }
+
+      top_artists_response = RestClient.get("https://api.spotify.com/v1/me/top/artists?limit=10", header)
+      top_artists_to_send = JSON.parse(top_artists_response.body)
+      render json: {top_artists: top_artists_to_send}
+    end
+end
