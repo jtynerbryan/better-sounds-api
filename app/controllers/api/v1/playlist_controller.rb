aa14de5ed197cdcd13b71aa09f461a61d7da211f
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-09-27 13:31:53 -0400

    created related_artists and playlist controllers

---
+++ app/controllers/api/v1/playlist_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::PlaylistController < ApplicationController
+
+  def get_playlist
+
+  end
+
+  def create_playlist
+
+  end
+
+  def add_tracks
+
+  end
+
+end
---
+++ app/controllers/api/v1/related_artists_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::RelatedArtistsController < ApplicationController
+
+  def related_artists
+
+  end
+
+  def top_tracks
+
+  end
+
+  
+
+end
