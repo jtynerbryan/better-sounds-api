06be33fff2f2af6e9ba754d263ac01481c71aa03
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2017-10-02 11:49:53 -0400

    successfully persisting playlists and rendering to frontend

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::PlaylistsController < ApplicationController
 
   def get_playlist
-
+    @user = User.find(params[:user_id])
+    @playlists = @user.playlists
   end
 
   def create_playlist
@@ -21,18 +22,21 @@ class Api::V1::PlaylistsController < ApplicationController
       api_response = RestClient.post(api_url, body.to_json, header)
       new_playlist = JSON.parse(api_response.body)
 
-      #add tracks to new playlist
+      #create new playlist in rails database
+      playlist = Playlist.create(spotify_id: new_playlist["id"], user_id: @user.id)
+
+      #split track uris to send to Spotify api
       track_body = {
         uris: params[:track_ids].split(',')
       }
-      
+
       tracks_api_url = "https://api.spotify.com/v1/users/#{@user.username}/playlists/#{new_playlist["id"]}/tracks"
 
       tracks_api_response = RestClient.post(tracks_api_url, track_body.to_json, header)
       parsed_tracks_api_response = JSON.parse(tracks_api_response.body)
 
-      #return new playlist with tracks to frontend
-      render json: {playlist: parsed_tracks_api_response}
+      #return new playlist to frontend
+      render json: {playlist: playlist}
   end
 
 end
