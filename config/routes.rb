64ea95ef032a1144fedcf2353badaea5172a5041
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-03-19 14:34:08 -0400

    made routes more RESTful

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
         get '/login', :to => 'auth#spotify_request'
         post '/users/create', :to => 'users#create'
         get '/users/show', :to => 'users#show'
-        post '/top_tracks', :to => 'tracks#top_tracks'
-        post '/recently_played_tracks', :to => 'tracks#recently_played_tracks'
-        post '/audio_features', :to => 'audio_features#audio_features'
-        post '/related_artists', :to => 'related_artists#related_artists'
-        post '/related_artists_top_tracks', :to => 'related_artists#top_tracks'
-        post '/top_artists', :to => 'top_artists#top_artists'
+        get '/top_tracks', :to => 'tracks#top_tracks'
+        get '/recently_played_tracks', :to => 'tracks#recently_played_tracks'
+        get '/audio_features', :to => 'audio_features#audio_features'
+        get '/related_artists', :to => 'related_artists#related_artists'
+        get '/related_artists_top_tracks', :to => 'related_artists#top_tracks'
+        get '/top_artists', :to => 'top_artists#top_artists'
         post '/create_playlist', :to => 'playlists#create'
-        post '/get_playlists', :to => 'playlists#show'
+        get '/get_playlists', :to => 'playlists#show'
       end
   end
 end
